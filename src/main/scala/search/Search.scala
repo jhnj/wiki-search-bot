ed2e60a876200a159510a775f6d9df7a651f7ac1
Johan Jern <johanjern95@gmail.com>
2017-10-01 00:35:03 +0300

    Better formatting of output

--- src/main/scala/search/Search.scala
+++ src/main/scala/search/Search.scala
@@ -3,6 +3,8 @@ package search
 import app.Config
 import cats.effect.IO
 import cats.implicits._
+import cats._
+import cats.data._
 
 
 class Search(implicit config: Config) {
@@ -13,30 +15,50 @@ class Search(implicit config: Config) {
     (seq, config) => {
       if (seq.length >= 2)
         searchGraph(seq.head, seq(1))(config)
-          .map(formatResult)
       else
         IO { "Need 2 parameters" }
     }
 
-  def searchGraph(from: String, to: String)(config: Config): IO[Option[List[String]]] = {
+  def searchGraph(from: String, to: String)(config: Config): IO[String] = {
     val db = new DB(config)
-    val offsets = for {
-      f <- db.getOffset(from)
-      t <- db.getOffset(to)
-    } yield (f,t).bisequence
-    val nodes = offsets.map { opt =>
-      opt.map { case (f,t) =>
+    val offsets: IO[Either[Seq[String], Seq[Int]]] = for {
+      l <- List(from, to).traverse(getOffsetOrError(db))
+    } yield sequenceEither(l)
+    val nodes = offsets.map { either =>
+      either.map { case List(f,t) =>
         searchGraph.bfs(f,t)
       }
     }
-    nodes.flatMap { opt =>
-      val list = opt.get
-      db.getTitles(list)
+    nodes.flatMap {
+        case Right(ofs) if ofs.length >= 2 =>
+          db.getTitles(ofs).map(formatResult)
+        case Left(notFound) => IO {
+          if (notFound.length == 2)
+            s"Could not find titles: ${notFound.head} and ${notFound(1)}"
+          else if (notFound.length == 1)
+            s"Could not find title: ${notFound.head}"
+          else "Something went wrong"
+      }
+    }
+  }
+
+  def getOffsetOrError(db: DB)(title: String): IO[Either[String, Int]] = {
+    db.getOffset(title).map {
+      case Some(offset) => Right(offset)
+      case None => Left(title)
     }
   }
 
   def formatResult(res: Option[List[String]]): String = {
-    res.map(_.toString).getOrElse("Path not found")
+    res.map { list =>
+      s"Shortest path between ${list.head} and ${list.last}:\n" +
+      list.mkString(" -> ")
+    } getOrElse "Path not found"
+  }
+
+  def sequenceEither[A,B](x: Seq[Either[A, B]]): Either[Seq[A], Seq[B]] = x partition {_.isLeft} match {
+    case (Seq(), r) => Right(r map {_.right.get})
+    case (l, _) => Left(l map {_.left.get})
   }
 }
 
